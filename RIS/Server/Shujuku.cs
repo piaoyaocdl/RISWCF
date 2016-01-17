0d42d66d1e47cef5e31708ece4f19a1345559c39
piaoyaocdl <piaoyaocdl@163.com>
2016-01-17 20:20:19 +0800

    1111111111111111111111111

---
+++ RIS/Server/Shujuku.cs
@@ -0,0 +1,18 @@
+﻿namespace Server
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Shujuku : DbContext
+    {
+        public Shujuku()
+            : base("name=Shujuku")
+        {
+        }
+
+        public virtual DbSet<Table.Yonghu> Yonghu { get; set; }
+    }
+
+   
+}
